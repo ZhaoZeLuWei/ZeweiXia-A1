e332e75456834bb71c2c71066a764d3086a319c1
Zewei Xia <xiazeweibobby@gmail.com>
2024-11-19 23:03:46 +0800

    Child Class OtherHealthProfessional
    Create the second child class OtherHealthProfessional

--- src/OtherHealthProfessional.java
+++ src/OtherHealthProfessional.java
@@ -1,3 +1,35 @@
-public class OtherHealthProfessional {
+public class OtherHealthProfessional extends HealthProfessional {
+    private String otherSpecialization;
+    private String doctorType = "Other Type Health Professional";
+
+    public OtherHealthProfessional(int id, String name, int age, String otherSpecialization) {
+        super(id, name, age);
+        this.otherSpecialization = otherSpecialization;
+    }
+
+    public OtherHealthProfessional(){
+        super();
+        this.otherSpecialization = "Other type";
+    }
+
+    @Override
+    public String toString() {
+        return  "Doctor Type: " + doctorType + 
+                "\nDetails are as follows\n" +
+                super.toString() +
+                "Specialization: " + otherSpecialization + "\n";
+    }
+
+    public String getOtherSpecialization() {
+        return otherSpecialization;
+    }
+
+    public String getDoctorType() {
+        return doctorType;
+    }
+
+    public void setOtherSpecialization(String newSpecialization) {
+        this.otherSpecialization = newSpecialization;
+    }    
 
 }
