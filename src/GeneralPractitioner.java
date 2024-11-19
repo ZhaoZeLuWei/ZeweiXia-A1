b2c21a67cf8245ec857c72b960eeffeeb2853ada
Zewei Xia <xiazeweibobby@gmail.com>
2024-11-19 22:52:38 +0800

    Child class GeneralPractitioner
    The first child class general practitioner created

--- src/GeneralPractitioner.java
+++ src/GeneralPractitioner.java
@@ -1,3 +1,36 @@
 public class GeneralPractitioner extends HealthProfessional {
+    private String doctorSpecialization;
+    private String doctorType = "General Practitioner";
+
+    public GeneralPractitioner(int id, String name, int age, String doctorSpecialization) {
+        super(id, name, age);
+        this.doctorSpecialization = doctorSpecialization;
+    }
+
+    public GeneralPractitioner(){
+        super();
+        this.doctorSpecialization = "General";
+    }
+
+    @Override
+    public String toString() {
+        return  "Doctor Type: " + doctorType + 
+                "\nDetails are as follows\n" +
+                super.toString() +
+                "Specialization: " + doctorSpecialization + "\n";
+    }
     
-}
+    public String getDoctorSpecialization() {
+        return doctorSpecialization;
+    }
+
+    public String getDoctorType() {
+        return doctorType;
+    }
+
+    public void setDoctorSpecialization(String doctorSpecialization) {
+        this.doctorSpecialization = doctorSpecialization;
+    }
+                
+ }
+
